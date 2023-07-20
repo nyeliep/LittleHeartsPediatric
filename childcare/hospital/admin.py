b3140167df1f25e678d7288ff5c0664b68a1d09c
nyeliep <nyeliep>
2023-07-20 07:49:45 +0300

    commit

---
+++ childcare/hospital/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import (Slider, Service, Item, Doctor, Expertize, Faq, Gallery)
+
+admin.site.register(Slider)
+admin.site.register(Service)
+admin.site.register(Item)
+admin.site.register(Doctor)
+admin.site.register(Expertize)
+admin.site.register(Faq)
+admin.site.register(Gallery)
